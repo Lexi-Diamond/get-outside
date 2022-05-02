be957c92dc368b975652f855618ab083513243b3
Lexi Dorrity <lexidorrity@Lexis-MacBook-Pro.local>
2022-05-02 16:12:09 -0400

    finished models

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,45 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const techSchema = new Schema({
-  name: {
+const userSchema = new Schema({
+  username: {
     type: String,
     required: true,
     unique: true,
+    trim: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+    },
+  ],
 });
 
-const Tech = model('Tech', techSchema);
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
 
-module.exports = Tech;
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
